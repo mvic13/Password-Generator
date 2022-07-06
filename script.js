40d205d28e15d16d0fd6e5127c89b226fd2429f4
mvic13 <106556270+mvic13@users.noreply.github.com>
2022-07-05 22:32:42 -0500

    Adding files

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
 function generatePassword() {
-  //TODO Generate Password and other logic here
-  
- generateBtn = prompt("How long do you want your password to be? please choose a number greater than 8 and less than 128.")
-  if (generateBtn < 8) {
-   alert ("Password length was not met") 
-  }
-  else {
-    confirm ("would you like lowercase?")
-  }
+  var password = "" ;
 
 
+//TODO How long is the password? Make this show up on the screen Should the user be able to type whatever they want? YES 1a. Check if less than 8. This would bad. 1b. Check if greater than 128. This would bad. 1c. Needs to be in between to be valid.
+var passwordLength = prompt("How long do you want your password to be?");
+passwordLength = parseInt(passwordLength);
 
+
+console.log (passwordLength);
+console.log (typeof passwordLength);
+
+if (passwordLength < 8 || passwordLength > 128) {
+  alert ("Length is invalid");
 }
+
+//Should the user be able to type whatever they want? No Only acceptable answers are yes or no...so boolean should suffice. Think of confirms.
+
+// Ask for lowercase
+var wantsLowercase = confirm ("Do you want lowercase");
+console.log(wantsLowercase)
+// Ask for uppercase
+var wantsUppercase = confirm ("Do you want Uppercase?");
+console.log(wantsUppercase)
+// Ask for numberic
+var wantsNumbers = confirm ("Do you want Numbers");
+console.log(wantsNumbers)
+// Ask for special characters
+var wantsSpecialCharacters = confirm ("Do you want SpecialCharacters");
+console.log(wantsSpecialCharacters);
+
+
+//Make sure user selects at least one of the four confirms. Otherwise you don't know what characters to use to make their password. Can't have a empty password.
+
+
+  return password ; 
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
