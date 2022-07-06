5fca4258bb12cd0e08be4d508037bef8166ae682
mvic13 <106556270+mvic13@users.noreply.github.com>
2022-07-05 22:25:44 -0500

    updated files

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword() {
+  //TODO Generate Password and other logic here
+  
+ generateBtn = prompt("How long do you want your password to be? please choose a number greater than 8 and less than 128.")
+  if (generateBtn < 8) {
+   alert ("Password length was not met") 
+  }
+  else {
+    confirm ("would you like lowercase?")
+  }
+
+
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
